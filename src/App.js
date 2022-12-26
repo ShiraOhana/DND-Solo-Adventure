364f984ce681e408a5ea721e2929f1684d44f345
ShiraOhana <soshiraohana@gmail.com>
2022-12-26 12:07:39 +0200

    basic css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
+import "./Style/App.css";
+import "./Style/normalize.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <header className="App-header">
         <h1>Single player DND</h1>
       </header>
