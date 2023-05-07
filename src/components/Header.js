d37824977071be833c24e07afb18449e39d9882e
ShiraOhana <soshiraohana@gmail.com>
2023-05-07 14:06:22 +0300

    Header navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export default function Header() {
+function Header() {
   return (
-    <h1 style={{ fontSize: "50px", color: "cornflowerblue", margin: "20px" }}>
-      Single player DND
-    </h1>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">D&D Solo Adventure</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
+export default Header;
